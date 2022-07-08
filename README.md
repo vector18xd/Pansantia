dea37b629ee194b6e69ddad4545abab1aeae381b
vector18xd <victxd18pepe@gmail.com>
2022-07-08 15:39:24 -0500

    esta bien

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nombre: Victor Manuel Ramirez Collantes
+# Documento: 1092850477
+# Documentos: Aca se guardara todo lo que tenga que ver con la empresa(Jardienes)
---
+++ VuePractica/Pratica/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Hola Mundo</p>
+    </div>
+</template>
+
+<script>
+import login from "./components/menu/login.vue"
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
